f82c9b93cef65479c1b7bd47d058712327ba8a19
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 03:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const line = document.getElementById("line");
+let rotation = 0;
+
+function rotateLine() {
+  rotation += 1;
+  line.style.transform = `rotate(${rotation}deg)`;
+}
+
+setInterval(rotateLine, 10);
